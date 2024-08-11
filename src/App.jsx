3e6e4182f2669e19f3f7cb5e815dc347403d6871
Olexandrd <olexandrd@gmail.com>
2024-08-11 20:21:01 +0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Toaster } from "react-hot-toast";
 import SearchBar from "./components/SearchBar/SearchBar";
 import { normalizeString } from "./helpers/string";
+import { useState } from "react";
+import { fetchImages } from "./helpers/api";
 
 const App = () => {
+  const [page, setPage] = useState(1);
+
   const searchHandler = (e) => {
     e.preventDefault();
-    console.log(normalizeString(e.target.searchQuery.value));
+    const searchQuery = normalizeString(e.target.searchQuery.value);
+
+    fetchImages({ query: searchQuery, page: page, per_page: 10 })
+      .then((data) => {
+        console.log(data);
+        setPage(page + 1);
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
---
+++ src/helpers/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.unsplash.com/";
+const VITE_UNSPLASH_API_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.headers.common[
+  "Authorization"
+] = `Client-ID ${VITE_UNSPLASH_API_KEY}`;
+
+export const fetchImages = async ({ query, page, per_page }) => {
+  const response = await axios.get(
+    `/search/photos?query=${query}&page=${page}&per_page=${per_page}&orientation=landscape`
+  );
+  return response.data;
+};
